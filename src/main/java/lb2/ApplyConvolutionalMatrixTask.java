3ea8a116de33201132ba09184067592ee938a798
Nikita <kuzliaev.nikita@yandex.ru>
2026-04-10 15:03:30 +0700

    lb-2: add ApplyConvolutionalMatrixTask 1. Создал реализацию ApplyConvolutionalMatrixTask интерфейса FrameTask (для поддержания единообразия, как в первой лабораторной работе) 2. Внутри реализации происходит конвертация BufferedImage в формат, понятный GpuFrameProcessor, происходит вызов process() у GpuFrameProcessor, результат обработки обратно собирается в BufferedImage

---
+++ src/main/java/lb2/ApplyConvolutionalMatrixTask.java
@@ -0,0 +1,42 @@
+package lb2;
+
+import common.FrameTask;
+import common.FrameCapsule;
+import common.FrameResult;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+
+import static common.BufferedImage2Bytes.*;
+
+public class ApplyConvolutionalMatrixTask implements FrameTask {
+
+    private final GpuFrameProcessor gpuFrameProcessor;
+
+    ApplyConvolutionalMatrixTask(GpuFrameProcessor gpuFrameProcessor){
+        this.gpuFrameProcessor = gpuFrameProcessor;
+    }
+
+    @Override
+    public FrameResult process(FrameCapsule frame) {
+        System.out.println("Enter in ApplyConvolutionalMatrixTask.process()");
+
+        BufferedImage input = frame.getImage();
+        BufferedImage image = ensure3ByteBGR(input);
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        byte[] bgr = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+
+        // BGR → RGB
+        byte[] rgb = bgrToRgb(bgr);
+        // GPU обработка
+        byte[] resultRgb = gpuFrameProcessor.process(rgb, width, height);
+        // RGB → BGR
+        byte[] resultBgr = rgbToBgr(resultRgb);
+
+        BufferedImage resultImage = buildImage(resultBgr, width, height);
+        return new FrameResult(frame.getFrameNumber(), resultImage, null);
+    }
+}
