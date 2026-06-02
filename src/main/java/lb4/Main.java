0ade5a48ca4446380339497cafee97f8b21773e3
DESKTOP-PHE61FP\nikita <kuzliaev.nikita@yandex.ru>
2026-06-02 14:31:25 +0700

    add filepath for map and output folder and run Pipeline with this params

--- src/main/java/lb4/Main.java
+++ src/main/java/lb4/Main.java
@@ -3,11 +3,10 @@ package lb4;
 public class Main {
 
     public static void main(String[] args) {
+        String mapPath = "src/main/java/lb4/citymap.txt";
+        String outputPath = "src/main/java/lb4/output";
 
-        Pipeline pipe = new Pipeline();
-
+        Pipeline pipe = new Pipeline(mapPath, outputPath);
         pipe.run();
-
     }
-
 }
